842f61d7752344f9ea3eb72ac7ee7d59e9a087e9
Leah <111544542+leiiiii1@users.noreply.github.com>
2025-03-13 06:40:07 +0800

    Add files via upload

--- GameManager.cs
+++ GameManager.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public int hp;
     public int score;
+
+    public void Update()
+    {
+        if(hp <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
 }
